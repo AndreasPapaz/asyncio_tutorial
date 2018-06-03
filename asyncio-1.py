951be852f45a21ca469fc9adcde9acb02bed9057
AndreasPapaz <andreaspapaz@gmail.com>
2018-06-03 15:58:39 -0500

    added spinner for threading

--- asyncio-1.py
+++ asyncio-1.py
@@ -17,13 +17,12 @@ async def main():
     divs2 = loop.create_task(find_divisibles(100052, 3210))
     divs3 = loop.create_task(find_divisibles(500, 3))
     await asyncio.wait([divs1, divs2, divs3])
-    return divs1, divs2, divs3
 
 
 if __name__ == '__main__':
     try:
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(mail())
+        loop.run_until_complete(main())
         loop.close()
     except:
         pass
